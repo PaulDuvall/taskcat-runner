d1ba088f3fca253508330235e7b67e3476fe50b4
Paul Duvall <pduvall@gmail.com>
2020-07-28 18:59:13 +0000

    Auto delete a stack

--- cfn-auto-delete.sh
+++ cfn-auto-delete.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Bash template based on https://github.com/eppeters/bashtemplate.sh
 # sudo chmod +x *.sh
-# ./create-cfn-role.sh
+# ./cfn-auto-delete.sh
 
 set -euo pipefail
 IFS=$'\n\t'
 
-#/ Usage: create-cfn-role.sh ROLE_NAME CFN_STACK_NAME
+#/ Usage: cfn-auto-delete.sh
 #/ Description: Create an IAM Role for a specific CloudFormation Stack 
 #/ Options:
 #/   --help: Display this help message
@@ -19,9 +19,16 @@ warning() { echo "[WARNING] $@" | tee -a "$LOG_FILE" >&2 ; }
 error()   { echo "[ERROR]   $@" | tee -a "$LOG_FILE" >&2 ; }
 fatal()   { echo "[FATAL]   $@" | tee -a "$LOG_FILE" >&2 ; exit 1 ; }
 
-ROLE_NAME=${1:-${ROLE_NAME:-}}
-if [ -z "$ROLE_NAME" ]; then
+STACK_NAME=${1:-${STACK_NAME:-}}
+if [ -z "$STACK_NAME" ]; then
     usage
 fi
 
-aws cloudformation create-stack --stack-name $ROLE_NAME --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://cfn-role.yml --parameters ParameterKey=MyRoleName,ParameterValue=$ROLE_NAME
+TTL=${2:-${TTL:-}}
+if [ -z "$TTL" ]; then
+    usage
+fi
+
+# aws cloudformation create-stack --stack-name cloudformation-admin-iam --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://cloudformation-admin-iam.yml
+
+aws cloudformation create-stack --stack-name ttl --capabilities CAPABILITY_NAMED_IAM --disable-rollback --template-body file://cloudformation-stack-ttl.yml --parameters ParameterKey=StackName,ParameterValue=$STACK_NAME ParameterKey=TTL,ParameterValue=$TTL
